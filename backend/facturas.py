09b280c9f38845749e885e04156524202aa2fc0d
Pedro Alejandro Zetino Paez <alezetino2002@gmail.com>
2021-11-03 21:57:21 -0600

    fuck

--- backend/facturas.py
+++ backend/facturas.py
@@ -1,4 +1,5 @@
 class Facturas():
+    file = ""
     listafec = []
     listaref = []
     listanite = []
@@ -8,22 +9,28 @@ class Facturas():
     listatot = []
     listacom = [] #Si el valor es 1, fue aprobada, si es 0 la reprobé Hora 4:13 am
     contadorvalidos = 0
+    ivaem = []
+    ivarec = []
     contadorerrores = 0
     contadordefacturas = 0
-    contadoremisores = 0
-    condaroreceptores = 0
+    contadoremisores = []
+    condaroreceptores = []
     erremisores = 0
     errreceptores = 0
     erriva = 0
     errref = 0
     errtotal = 0
-    def __init__(self):
-        print("Hola")
+    def __init__(self,fecha,ref,nitre,nitem,valor,iva,total,compro):
+        self.fecha = fecha
+        self.ref = ref
+        self.nitre = nitre
+        self.nitem = nitem
+        self.valor = valor
+        self.iva = iva
+        self.total = total
+        self.compro = compro
 
-
-
-
-    def getnit(nit):
+    def crear(self,fecha,ref,nitre,nitem,valor,iva,total,compro):
         print("matat")
         return "hola"
     
--- backend/manage.py
+++ backend/manage.py
@@ -1,5 +1,14 @@
 from facturas import Facturas
-
 class Manage():
     def __init__(self):
         self.nit =  []
+
+    def duplicadasref(ref):
+        b = ref
+        a = len(Facturas.listaref)
+        for i in range(a):
+            if b == Facturas.listaref[i]:
+                Facturas.errref +=1
+            i =+ 1
+        
+        return False
